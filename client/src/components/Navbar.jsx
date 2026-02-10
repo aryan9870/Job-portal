8e1d4264f828158e8d31d44d5454ab7d0de154e9
Aryan <aryan7017n@gmail.com>
2026-02-10 17:05:22 +0530

    Add navbar UI

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets'
+
+const Navbar = () => {
+  return (
+    <div className='shadow py-4'>
+        <div className='container px-10 mx-auto flex justify-between items-center'>
+            <img className='cursor-pointer' src={assets.logo} alt="" />
+            <div className='flex gap-4'>
+                <button className='text-gray-600 cursor-pointer'>Recruiter Login</button>
+                <button className='bg-blue-600 cursor-pointer text-white px-9 py-2 rounded-full'>Login</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
