8715f413b931aa616e8d988d00c40da3ad48d7a2
Aryan <aryan7017n@gmail.com>
2026-02-10 23:29:40 +0530

    Add job search functionality using Context API

---
+++ client/src/assets/assets.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import search_icon from './search_icon.svg';
+import location_icon from './location_icon.svg';
+import accenture_logo from './accenture_logo.png';
+import adobe_logo from './adobe_logo.png';
+import amazon_logo from './amazon_logo.png';
+import microsoft_logo from './microsoft_logo.svg';
+import samsung_logo from './samsung_logo.png';
+import walmart_logo from './walmart_logo.svg';
+
+
+export const assets = {
+    logo,
+    search_icon,
+    location_icon,
+    accenture_logo,
+    adobe_logo,
+    amazon_logo,
+    microsoft_logo,
+    samsung_logo,
+    walmart_logo,
+    
+}
