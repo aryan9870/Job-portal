083f3d5e121b8f884d4418ac281908594343fbbc
Aryan <aryan7017n@gmail.com>
2026-02-15 19:39:28 +0530

    Setup dashboard nested routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import Applictions from "./pages/Applictions";
 import RecruiterLogin from "./components/RecruiterLogin";
 import { AppContext } from "./context/AppContext";
 import UserLogin from "./components/UserLogin"
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/manageJobs";
+import ViewApplications from "./pages/ViewApplications";
 
 
 const App = () => {
@@ -20,6 +24,11 @@ const App = () => {
         <Route path="/" element={<Home />}/>
         <Route path="/apply-job/:id" element={<Applyjob />}/>
         <Route path="/applications" element={<Applictions />}/>
+        <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="add-job" element={<AddJob />}/>
+            <Route path="manage-jobs" element={<ManageJobs />}/>
+            <Route path="view-applications" element={<ViewApplications />}/>
+        </Route>
       </Routes>
     </div>
   );
