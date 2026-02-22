2996ab7a3bf46c8107bbf1f2197d6e5836ec1924
Aryan <aryan7017n@gmail.com>
2026-02-22 15:50:11 +0530

    Implement recruiter application management with status update functionality

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,7 +1,49 @@
-import React from "react";
-import { applicationsData, assets } from "../assets/assets";
+import React, { useContext, useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import axios from "axios";
+import { AppContext } from "../context/AppContext";
+import { AlertContext } from "../context/AlertContext";
 
 const ViewApplications = () => {
+  const { backendUrl } = useContext(AppContext);
+  const { showAlert } = useContext(AlertContext);
+  const [applications, setApplications] = useState([]);
+
+  const fetchApplications = async () => {
+    try {
+      const { data } = await axios.get( 
+        backendUrl + "/api/applications/recruiter",
+        { withCredentials: true },
+      );
+      if (data.success) {
+        setApplications(data.applications);
+      }
+    } catch (error) {
+      console.log(error.responce.data.message);
+    }
+  };
+
+  const updateApplicationStatus = async (id, status) => {
+    try {
+      const { data } = await axios.patch(
+        backendUrl + `/api/applications/${id}`,
+        { status },
+        { withCredentials: true },
+      );
+      if (data.success) {
+        showAlert(data.message, "success");
+        fetchApplications();
+      }
+    } catch (error) {
+      showAlert(error?.responce?.data?.message, "error")
+    }
+  };
+
+  useEffect(() => {
+    fetchApplications();
+  }, []);
+
+  
   return (
     <div className="container mx-auto p-4">
       <div>
@@ -12,38 +54,66 @@ const ViewApplications = () => {
               <th className="py-2 px-4 text-left">User name</th>
               <th className="py-2 px-4 text-left">Job Title</th>
               <th className="py-2 px-4 text-left">Location</th>
+              <th className="py-2 px-4 text-left">Status</th>
               <th className="py-2 px-4 text-left">Resume</th>
               <th className="py-2 px-4 text-left">Action</th>
             </tr>
           </thead>
           <tbody>
-            {applicationsData.map((applicant, index) => {
+            {applications.map((application, index) => {
               return (
                 <tr key={index} className="text-gray-700">
                   <td className="py-2 px-4 border-b text-center border-gray-200">
                     {index + 1}
                   </td>
-                  <td className="py-2 px-4 border-b text-center flex border-gray-200">
+                  <td className="py-2 px-4 border-b flex justify-center items-center border-gray-200">
                     <img
-                      className="w-10 h-10 rounded-full mr-3"
-                      src={applicant.avatar}
+                      className="w-8 h-8 rounded-full mr-3"
+                      src={application.applicant.image}
                       alt=""
                     />
-                    <span>{applicant.userName}</span>
+                    <span>{application.applicant.name}</span>
                   </td>
-                  <td className="py-2 px-4 border-b border-gray-200">{applicant.jobTitle}</td>
-                  <td className="py-2 px-4 border-b border-gray-200">{applicant.location}</td>
                   <td className="py-2 px-4 border-b border-gray-200">
-                    <a href="" target="_blank" className="bg-blue-50 text-blue-400 py-1 px-2 rounded inline-flex gap-2 items-center">
+                    {application.job.title}
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-200">
+                    {application.job.location}
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-200">
+                    <span className={`${application.status == "accepted" ? 'bg-green-100' : application.status == "rejected" ? 'bg-red-100' : 'bg-blue-100' } px-2 py-1 rounded`}>{application.status}</span>
+                  </td>
+                  <td className="py-2 px-4 border-b border-gray-200">
+                    <a
+                      href=""
+                      target="_blank"
+                      className="bg-blue-50 text-blue-400 py-1 px-2 rounded inline-flex gap-2 items-center"
+                    >
                       Resume <img src={assets.resume_download_icon} alt="" />
                     </a>
                   </td>
                   <td className="py-2 px-4 border-b relative border-gray-200">
                     <div className="relative inline-block text-left group">
-                      <button className="text-gray-500 action-button text-3xl">...</button>
+                      <button className="text-gray-500 action-button text-3xl">
+                        ...
+                      </button>
                       <div className="z-10 hidden absolute ring-0 left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block">
-                        <button className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100">Accept</button>
-                        <button className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100">Reject</button>
+                        <button
+                          onClick={() =>
+                            updateApplicationStatus(application._id, "accepted")
+                          }
+                          className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100"
+                        >
+                          Accept
+                        </button>
+                        <button
+                          onClick={() =>
+                            updateApplicationStatus(application._id, "rejected")
+                          }
+                          className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100"
+                        >
+                          Reject
+                        </button>
                       </div>
                     </div>
                   </td>
