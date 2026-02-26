5dbcf1f6665e0b92549bd4945cfe77dd659412f6
Aryan <aryan7017n@gmail.com>
2026-02-26 15:02:51 +0530

    configure production environment for render deployment

--- server/server.js
+++ server/server.js
@@ -4,16 +4,20 @@ import connectDB from "./config/db.js";
 import ErrorHandler from "./utils/errorHandler.js";
 import userRouter from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
-import jobRouter from "./routes/jobRoute.js"
-import applicationRouter from "./routes/applicationRoutes.js"
+import jobRouter from "./routes/jobRoute.js";
+import applicationRouter from "./routes/applicationRoutes.js";
 connectDB();
 
 const app = express();
 
 // Middleware
-const allowedOrigins = ['http://localhost:5173']
 
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -26,27 +30,23 @@ app.get("/", (req, res) => {
   });
 });
 
-
 app.use("/api/users", userRouter);
 app.use("/api/jobs", jobRouter);
 app.use("/api/applications", applicationRouter);
 
-
-// Route not found 
+// Route not found
 app.use((req, res, next) => {
   return next(new ErrorHandler(`Route ${req.originalUrl} not found`, 404));
 });
-
 
 // Error handler middleware
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(err.statusCode || 500).json({
     success: false,
-    message: err.message
+    message: err.message,
   });
 });
-
 
 // Server Listen
 const PORT = process.env.PORT || 5000;
